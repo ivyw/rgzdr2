5951e617090b12576b01a610bd2912250a672135
MatthewJA <matthew.alger@gmail.com>
2025-05-12 23:37:07 +1000

    Code cleanup for subjects.py

--- rgz/subjects.py
+++ rgz/subjects.py
@@ -1,5 +1,6 @@
 """Handles RGZ subjects."""
 
+from collections.abc import Sequence
 import json
 import logging
 from pathlib import Path
@@ -11,11 +12,13 @@ import astroquery
 from astroquery.image_cutouts.first import First
 from astropy.io import fits
 import astropy.units as u
+from astropy.units import Quantity
 from astroquery.vizier import Vizier
 from astropy.wcs import WCS, FITSFixedWarning
 import attr
 import backoff
 import numpy as np
+import numpy.typing as npt
 import requests
 from tqdm import tqdm
 
@@ -30,12 +33,26 @@ MAX_TRIES = 10
 logger = logging.getLogger(__name__)
 
 
+type BBox = tuple[float, float, float, float]
+
+
 @attr.s
 class Subject:
-    id = attr.ib()
-    zid = attr.ib()
-    coords = attr.ib()
-    bboxes = attr.ib()
+    """A Radio Galaxy Zoo subject.
+
+    Attributes:
+        id: RGZ MongoDB ID.
+        zid: Zooniverse ID.
+        coords: Central right ascension, declination, both in degrees.
+        bboxes: Bounding boxes for the radio islands in the subject.
+                This is defined per Radio Galaxy Zoo, so
+                (xmin, ymin, xmax, ymax).
+    """
+
+    id: str = attr.ib()
+    zid: str = attr.ib()
+    coords: tuple[float, float] = attr.ib()
+    bboxes: Sequence[BBox] = attr.ib()
 
 
 @backoff.on_exception(
@@ -43,12 +60,15 @@ class Subject:
     requests.exceptions.ConnectionError,
     max_tries=MAX_TRIES,
 )
-def fetch_first(coord: SkyCoord, image_size: u.arcmin) -> fits.HDUList:
-    """Fetches a FIRST image from the FIRST server."""
+def download_first(coord: SkyCoord, image_size: Quantity[u.arcmin]) -> fits.HDUList:
+    """Downloads a FIRST image from the FIRST server."""
     return First.get_images(coord, image_size=image_size)
 
 
-def download_first_image(raw_subject: dict[str, Any], cache: Path) -> fits.HDUList:
+def fetch_first_from_server_or_cache(
+    raw_subject: dict[str, Any],
+    cache: Path,
+) -> fits.HDUList:
     """Fetches a FIRST image from the FIRST server or cache."""
     coord = raw_subject["coords"]
     coord = SkyCoord(ra=coord[0], dec=coord[1], unit="deg")
@@ -57,7 +77,7 @@ def download_first_image(raw_subject: dict[str, Any], cache: Path) -> fits.HDULi
     try:
         return fits.open(fname)
     except FileNotFoundError:
-        im = fetch_first(coord, image_size=3 * u.arcmin)
+        im = download_first(coord, image_size=3 * u.arcmin)
         im.writeto(fname)
         return im
 
@@ -66,32 +86,35 @@ def transform_coord_radio(
     coord: tuple[int, int],
     raw_subject: dict[str, Any],
     cache: Path,
-) -> u.Quantity:
-    """Transforms a radio image pixel coordinate.
+) -> Quantity[u.deg, u.deg]:
+    """Transforms a radio image pixel coordinate into RA/dec.
 
     Note that this uses the WCS of the subject image, and can be slow!
 
     TODO: Speed this up by avoiding the image reload whenever possible, e.g. by passing in the image.
     """
-    im = download_first_image(raw_subject, cache)
+    im = fetch_first_from_server_or_cache(raw_subject, cache)
     header = im[0].header
-    # WCS.dropaxis doesn't seem to work on these images
-    # drop these: CTYPE3 CRVAL3 CDELT3 CRPIX3 CROTA3
+    # WCS.dropaxis doesn't seem to work on these images.
+    # Drop these: CTYPE3 CRVAL3 CDELT3 CRPIX3 CROTA3...
     for key in ["CTYPE", "CRVAL", "CDELT", "CRPIX", "CROTA"]:
         for i in [3, 4]:
             del header[key + str(i)]
 
     with warnings.catch_warnings(action="ignore", category=FITSFixedWarning):
         wcs = WCS(header)
-    # coord in 132x132 -> 100x100
+
+    # Coord in 132x132 -> 100x100.
     coord = coord * 100 / 132
-    # flip y axis
+
+    # Flip y axis.
     c = wcs.all_pix2world([coord], 0)[0] * u.deg
+
     return c
 
 
-def transform_bbox(bbox, raw_subject, cache):
-    bbox_ = bbox
+def transform_bbox(bbox: BBox, raw_subject: dict[str, Any], cache: Path) -> npt.NDArray:
+    """Transforms a bbox from pixel coordinates to RA/dec."""
     bbox = np.array(bbox)
     bbox = np.concatenate(
         [
@@ -102,21 +125,25 @@ def transform_bbox(bbox, raw_subject, cache):
     return bbox
 
 
-def get_first_from_bbox(bbox, raw_subject, cache, verbose=False):
-    # TODO: might need to flip horizontally or even vertically...
+def get_first_from_bbox(
+    bbox: BBox,
+    raw_subject: dict[str, Any],
+    cache: Path,
+) -> list[str]:
+    """Finds FIRST components within a bounding box."""
+    # TODO: Might need to flip horizontally or even vertically...
     bbox = transform_bbox(bbox, raw_subject, cache)
-    # find the centre
+    # Find the centre...
     centre = (bbox[::2].mean(), bbox[1::2].mean())
-    # and the width, height
+    # ...and the width and height.
     width = abs(bbox[2] - bbox[0]).to(u.arcsec)
     height = abs(bbox[3] - bbox[1]).to(u.arcsec)
 
-    # round widths and heights up to nearest arcsec plus two
+    # Round widths and heights up to nearest arcsec plus two.
     width = np.ceil(width.to(u.arcsec)) + 2 * u.arcsec
     height = np.ceil(height.to(u.arcsec)) + 2 * u.arcsec
 
-    if verbose:
-        print("get_first_from_bbox:", centre, width, height)
+    logger.debug("get_first_from_bbox: %s %s %s", centre, width, height)
     skc = SkyCoord(
         ra=centre[0].value,
         dec=centre[1].value,
@@ -137,7 +164,7 @@ def get_first_from_bbox(bbox, raw_subject, cache, verbose=False):
 def get_bboxes(
     subject: dict[str, Any],
     cache: Path,
-) -> tuple[tuple[float, float, float, float], Any]:
+) -> Sequence[BBox]:
     """Fetches the bboxes of a subject from RGZ, caching locally."""
     fname = cache / f'{subject["_id"]["$oid"]}.json'
     try:
@@ -162,6 +189,7 @@ def get_bboxes(
 
 
 def subject_to_json_serialisable(subject: Subject) -> dict[str, Any]:
+    """Converts a Subject into a JSON-compatible dictionary."""
     return {
         "id": subject.id,
         "zid": subject.zid,
@@ -171,21 +199,23 @@ def subject_to_json_serialisable(subject: Subject) -> dict[str, Any]:
 
 
 def process_subject(
-    raw_subject: dict[str, Any], cache: Path, verbose: bool = False
+    raw_subject: dict[str, Any],
+    cache: Path,
 ) -> Subject:
+    """Reduces a JSON subject into a nice, value-added format."""
     sid = raw_subject["_id"]["$oid"]
     zid = raw_subject["zooniverse_id"]
     bboxes = get_bboxes(raw_subject, cache)
     bbox_to_firsts = {}
     for bbox in bboxes:
-        firsts = get_first_from_bbox(bbox, raw_subject, cache, verbose=verbose)
+        firsts = get_first_from_bbox(bbox, raw_subject, cache)
         bbox_to_firsts[bbox] = firsts
     s = Subject(id=sid, zid=zid, coords=raw_subject["coords"], bboxes=bbox_to_firsts)
     return s
 
 
-def process(subjects_path: Path, cache: Path, output_path: Path) -> Generator[Subject]:
-    """Process subjects from raw to reduced JSON."""
+def process(subjects_path: Path, cache: Path, output_path: Path):
+    """Processes subjects from raw to reduced JSON."""
     subjects = []
     # Get subject count for progress bar.
     with open(subjects_path, encoding="utf-8") as f:
@@ -205,7 +235,8 @@ def process(subjects_path: Path, cache: Path, output_path: Path) -> Generator[Su
         json.dump(json_subjects, f)
 
 
-def deserialise_subject(subject):
+def deserialise_subject(subject: dict[str, Any]) -> Subject:
+    """Reads a Subject from JSON."""
     return Subject(
         subject["id"],
         subject["zid"],
