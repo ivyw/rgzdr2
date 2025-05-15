57bec9b627837dc0b8df6e91bb9bed35f049a311
MatthewJA <matthew.alger@gmail.com>
2025-05-15 22:39:50 +1000

    More tidying

--- rgz/classifications.py
+++ rgz/classifications.py
@@ -24,41 +24,71 @@ logger = logging.getLogger(__name__)
 type JSON = dict[str, Any]
 
 
-def get_classifications(path="radio_classifications.json") -> Generator[JSON]:
-    """Yields classifications from RGZ."""
+def get_classifications(path: Path) -> Generator[JSON]:
+    """Yields classifications from RGZ.
+
+    Args:
+        path: Path to RGZ classifications JSON dumped from MongoDB.
+            Each line should be a separate JSON document.
+
+    Yields:
+        Classification JSON documents.
+
+    Raises:
+        ValueError if a classification document is invalid.
+    """
     with open(path, encoding="utf-8") as f:
         # Each row is a JSON document.
         for row in f:
             js = json.loads(row)
-            # if js['subject_ids'][0]['$oid'] not in all_subjects:
-            #     # sometimes not true??
-            #     continue
             for anno in js["annotations"]:
                 if "radio" in anno:
                     break
             else:
-                # no annotations?
-                continue
+                raise ValueError(f"Classification has no annotations: {js}")
             yield js
 
 
 @attr.s
 class Classification:
-    """A single RGZ classification of a subject by a citizen scientist."""
+    """A single RGZ classification of a subject by a citizen scientist.
+
+    Attributes:
+        cid: Classification MongoDB ID.
+        zid: Zooniverse ID of the subject being classified.
+        matches: Mapping from IR host name to radio component names.
+        username: Username of the citizen scientist.
+        notes: Extra tags assigned to this classification, possibly
+            during reduction.
+    """
 
     cid: str = attr.ib()
     zid: str = attr.ib()
     matches: list[tuple[str, set[str]]] = attr.ib()
-    username: str = attr.ib()
+    username: str | None = attr.ib()
     notes: list[str] = attr.ib()
 
 
 def transform_coord_ir(
     coord: npt.NDArray[np.float64],
     raw_subject: JSON | None = None,
-    wcs: astropy.wcs.WCS | None = None,
     cache: Path | None = None,
-):
+    wcs: astropy.wcs.WCS | None = None,
+) -> u.Quantity[u.deg, u.deg]:
+    """Transforms a coordinate from raw classifications to physical.
+
+    You can pass a subject and cache, or a WCS.
+
+    Args:
+        coord: Coord to transform, in px coordinates (0, IR_MAX_PX).
+        raw_subject: Raw JSON subject.
+        cache: Path to the subject data location.
+        wcs: WCS of the subject being classified.
+
+    Returns:
+        Transformed coordinate RA/dec in deg.
+    """
+    # TODO: We should use subjects here, not raw subjects.
     if not raw_subject and not wcs:
         raise ValueError()
     if raw_subject and not cache:
@@ -69,9 +99,9 @@ def transform_coord_ir(
         im = subjects.fetch_first_from_server_or_cache(raw_subject, cache)
         wcs = rgz.get_wcs(im, cache)
     assert wcs
-    # coord in 424x424 -> 424x424
+    # Coord in 424x424 -> 100x100
     px_coord = np.array(coord) * 100 / constants.IR_MAX_PX
-    # flip y axis?
+    # Flip y axis.
     px_coord[1] = 100 - px_coord[1]
     return wcs.all_pix2world([px_coord], 0)[0] * u.deg
 
@@ -80,14 +110,25 @@ def process_classification(
     raw_classification: JSON,
     subject: subjects.Subject,
     wcs: astropy.wcs.WCS,
-    defer_ir_lookup: bool = False,
+    defer_ir_lookup: bool = True,
 ) -> Classification:
-    """Converts a raw classification into a Classification."""
+    """Reduces a JSON classification into a nice, value-added format.
+
+    Args:
+        raw_classification: JSON classification.
+        subject: Subject being classified.
+        wcs: WCS of the subject image.
+        defer_ir_lookup: Whether to defer the IR lookup for later, or
+            perform the lookup in Vizier now (slow!).
+
+    Returns:
+        Reduced classification.
+    """
     cid = raw_classification["_id"]["$oid"]
     zid = raw_classification["subjects"][0]["zooniverse_id"]
     if zid != subject.zid:
         raise ValueError("Mismatched subjects.")
-    matches = []  # (wise, first)
+    matches: list[tuple[str, set[str]]] = []  # (wise, first)
     notes: list[str] = []
     for anno in raw_classification["annotations"]:
         if "radio" not in anno:
@@ -134,7 +175,7 @@ def process_classification(
         cid=cid,
         zid=zid,
         matches=matches,
-        username=raw_classification.get("user_name", constants.NO_USER_NAME),
+        username=raw_classification.get("user_name", None),
         notes=notes,
     )
 
@@ -143,8 +184,10 @@ def classification_to_json_serialisable(classification):
     return {
         "id": classification.cid,
         "zid": classification.zid,
-        "matches": [{"ir": ir, "radio": list(radio)} for ir, radio in classification.matches],
-        "username": classification.username,
+        "matches": [
+            {"ir": ir, "radio": list(radio)} for ir, radio in classification.matches
+        ],
+        "username": classification.username or constants.ANONYMOUS_NAME,
         "notes": classification.notes,
     }
 
@@ -153,7 +196,7 @@ def deserialise_classification(classification):
     return Classification(
         cid=classification["id"],
         zid=classification["zid"],
-        username=classification["username"],
+        username=classification["username"] or None,
         notes=classification["notes"],
         matches=[(m["ir"], set(m["radio"])) for m in classification["matches"]],
     )
