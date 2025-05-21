473426967616d13100a366e8665fece943c48599
MatthewJA <matthew.alger@gmail.com>
2025-05-21 18:25:22 +1000

    Dependency juggling

---
+++ rgz/subjects_test.py
@@ -0,0 +1,15 @@
+"""Tests for processing RGZ subjects."""
+
+import unittest
+
+import rgz.subjects
+
+
+class TestProcess(unittest.TestCase):
+    """Tests for rgz.subjects.process."""
+
+    def test_regression(self):
+        """Tests behaviour consistency in processing subjects."""
+        
+        
+        
