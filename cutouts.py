af36b02d31c4d650c44630322e6035df0f0f5e3b
Henry Zovaro <henry.zovaro@anu.edu.au>
2025-05-24 22:24:57 +1000

    Added docstring; now optionally saves FITS image to file

--- cutouts.py
+++ cutouts.py
@@ -2,6 +2,7 @@ import urllib.error
 from astropy.io import fits
 from astropy.coordinates import SkyCoord
 from astropy.table import Table
+import logging
 from pathlib import Path 
 import pandas as pd
 import tempfile
@@ -9,14 +10,53 @@ import urllib
 import shutil
 import sys 
 
+logger = logging.getLogger(__name__)
+
 def get_allwise_cutout(coords: SkyCoord,
-                       size_arcmin: int = 3,
+                       size_arcmin: float = 3,
                        band: str = "W1",
+                       save_fits: bool = False,
                        cutout_fname: Path = None,
-                       ):
+                       ) -> fits.HDUList:
     """Downloads a FITS cutout from AllWISE. 
+
+    This function extracts a cutout image from AllWISE via the NASA/IPAC 
+    Infrared Science Archive (IRSA). This is a two-step process: first, a
+    Simple Image Access Query is used to identify the list of AllWISE images 
+    containing the requested coordinates. The image from which to extract the 
+    cutout is taken as the first image in the list in the requested band 
+    that is also of "science" quality. Then, a cutout from the image is 
+    extracted by constructing a query using the corresponding access URL.
+
+    INPUTS
+    ---------------------------------------------------------------------------
+    coords          astropy.Skycoord
+        Requested cutout coordinates. 
     
-    References:
+    size_arcmin     float 
+        Requested cutout size in arcminutes. 
+    
+    band            str (default: "W1")
+        AllWISE band. Default: W1.
+    
+    save_fits       bool (default: False)
+        If True, saves the downloaded cutout to path specified by cutout_fname.
+        Otherwise, the FITS file is saved to a temporary file. 
+    
+    cutout_fname    Path
+        If save_fits is True, specifies the path where the image is saved. 
+        If unspecified, the file is saved to 
+            f"allwise_{band:s}_{ra_deg:.4f}_{dec_deg:.4f}.fits"
+        where ra_deg and dec_deg are the RA and dec respectively.
+    
+    RETURNS
+    ---------------------------------------------------------------------------
+    hdulist     astropy.io.fits.HDUList
+        HDUList object containing the FITS image and header.    
+    
+    REFERENCES 
+    ---------------------------------------------------------------------------
+        https://wise2.ipac.caltech.edu/docs/release/allwise/ 
         https://irsa.ipac.caltech.edu/ibe/docs/wise/allwise/p3am_cdd/ 
         https://irsa.ipac.caltech.edu/ibe/sia.html 
         https://irsa.ipac.caltech.edu/ibe/cutouts.html 
@@ -36,8 +76,11 @@ def get_allwise_cutout(coords: SkyCoord,
     dec_deg = coords.dec.value
     if cutout_fname is None:
         cutout_fname = Path(f"allwise_{band:s}_{ra_deg:.4f}_{dec_deg:.4f}.fits")
-    elif cutout_fname.suffix == "":
-        cutout_fname = cutout_fname.with_suffix(".fits")
+    else:
+        if save_fits == False:
+            logger.warning("You have specified a cutout_fname but I am not saving the result to file!")
+        if cutout_fname.suffix == "":
+            cutout_fname = cutout_fname.with_suffix(".fits")
 
     # Get list of AllWISE images containing the target RA/Dec, save to a temporary file 
     # TODO do we want CALIB=2 here?
@@ -47,9 +90,8 @@ def get_allwise_cutout(coords: SkyCoord,
             with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
                 shutil.copyfileobj(response, tmp_file)
     except urllib.error.URLError as e:
-        # TODO add logger 
         msg = f"Simple Image Access Query failed with message {e.message}!"
-        print(msg)
+        logger.warning(msg)
         return
 
     t = fits.open(tmp_file.name)
@@ -60,9 +102,8 @@ def get_allwise_cutout(coords: SkyCoord,
     cond = df["energy_bandpassname"] == band
     cond &= df["dataproduct_subtype"].str.rstrip() == "science"
     if df.loc[cond].shape[0] == 0:
-        # TODO add logger 
         msg = f"AllWISE cutout query for RA = {ra_deg:.4f}, dec = {dec_deg:.4f}, band = {band} returned no valid images!"
-        print(msg)
+        logger.warning(msg)
         return 
 
     # Get the access URL for the image. If there are multiple then just take the first one 
@@ -72,13 +113,19 @@ def get_allwise_cutout(coords: SkyCoord,
     query_str = f"center={ra_deg:.5f},{dec_deg:.5f}deg&size={size_arcmin:d}arcmin"
     cutout_url = f"{access_url}?{query_str}"
     try:
-        urllib.request.urlretrieve(cutout_url, cutout_fname)
+        if save_fits:
+            urllib.request.urlretrieve(cutout_url, cutout_fname)
+            hdulist = fits.open(cutout_fname)
+        else:
+            with urllib.request.urlopen(cutout_url) as response:
+                with tempfile.NamedTemporaryFile(delete=True) as tmp_file:
+                    shutil.copyfileobj(response, tmp_file)
+                    hdulist = fits.open(tmp_file.name)
     except urllib.error.URLError as e:
-        # TODO add logger
         msg = f"Cutout download failed with message {e.message}!"
-        print(msg)
+        logger.warning(msg)
 
-    return 
+    return hdulist
 
 
 if __name__ == "__main__":
@@ -93,15 +140,22 @@ if __name__ == "__main__":
 
     fname = Path("cutouts/ngc1068.fits")
     coords = SkyCoord(ra="02:42:40.71", dec="-00:00:47.86", unit=(u.hourangle, u.deg), equinox="J2000")
-    get_allwise_cutout(coords=coords,
-                       size_arcmin=10,
-                       cutout_fname=fname,)
-    
-    hdulist = fits.open(fname)
+    hdulist = get_allwise_cutout(coords=coords,
+                                 size_arcmin=10)
     im = hdulist[0].data 
-
-    fig, ax = plt.subplots(subplot_kw=dict(projection=WCS(hdulist[0].header)))
+    wcs = WCS(hdulist[0].header)
+    fig, ax = plt.subplots(subplot_kw=dict(projection=wcs))
     ax.imshow(np.log10(im))
 
-
+    # Saving to FITS file 
+    cutout_fname = Path("cutouts/NGC3997.fits")
+    coords = SkyCoord(ra="11:57:47.0", dec="+25:16:14.00", unit=(u.hourangle, u.deg), equinox="J2000")
+    hdulist = get_allwise_cutout(coords=coords,
+                                 size_arcmin=10,
+                                 save_fits=True, 
+                                 cutout_fname=cutout_fname)
+    im = hdulist[0].data 
+    wcs = WCS(hdulist[0].header)
+    fig, ax = plt.subplots(subplot_kw=dict(projection=wcs))
+    ax.imshow(np.log10(im))
 
