b34a966ef6d722132d624e51a06cb5cbac47340d
Henry Zovaro <henry.zovaro@anu.edu.au>
2025-07-03 21:28:42 +1000

    __main__ now plots NGC1068 (moved from cutouts_test.py)

--- rgz/cutouts.py
+++ rgz/cutouts.py
@@ -146,3 +146,19 @@ def get_allwise_cutout(
         hdulist.writeto(cutout_path, overwrite=True)
 
     return hdulist
+
+
+if __name__ == "__main__":
+    from astropy.wcs import WCS
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    # Plot the AllWISE cutout for NGC1068
+    coords = SkyCoord(
+        ra="02:42:40.71", dec="-00:00:47.86", unit=(u.hourangle, u.deg), equinox="J2000"
+    )
+    hdulist = get_allwise_cutout(coords=coords, size=3.5 * u.arcmin)
+    im = hdulist[0].data
+    wcs = WCS(hdulist[0].header)
+    fig, ax = plt.subplots(subplot_kw=dict(projection=wcs))
+    ax.imshow(np.log10(im))
