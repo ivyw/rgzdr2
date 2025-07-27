b2f8717660c935a0344cbd222b2f3791ae774d21
Henry Zovaro <henry.zovaro@anu.edu.au>
2025-07-27 17:58:06 +1000

    Moved code that queries SIA to get the list of AllWISE images into its own function (get_allwise_image_list) so that it can be mocked and tested

--- rgz/cutouts.py
+++ rgz/cutouts.py
@@ -1,5 +1,6 @@
 from io import BytesIO
 import logging
+import numpy as np
 from pathlib import Path
 from typing import Literal
 
@@ -9,6 +10,8 @@ from astropy import units as u
 from astropy.io import fits
 from astropy.coordinates import SkyCoord
 from astropy.table import Table
+import pandas as pd
+
 
 logger = logging.getLogger(__name__)
 
@@ -33,6 +36,54 @@ class InvalidWISEBandError(ValueError):
     """Raised when the requested band is invalid."""
 
 
+def get_allwise_image_list(
+     ra_deg: float,
+     dec_deg: float,
+     band: Literal["W1", "W2", "W3", "W4"],
+) -> pd.DataFrame:
+    """Returns a Pandas DataFrame containing a list of AllWISE images containing the specified coordinates.
+
+    This function uses a Simple Image Access Query to obtain a list of 
+    "science"-quality AllWISE images in the selected band containing the 
+    specified coordinates within a 0.01 degree radius. 
+
+    References:
+        https://wise2.ipac.caltech.edu/docs/release/allwise/
+        https://irsa.ipac.caltech.edu/ibe/docs/wise/allwise/p3am_cdd/
+        https://irsa.ipac.caltech.edu/ibe/sia.html
+        https://irsa.ipac.caltech.edu/ibe/cutouts.html
+
+    Args:
+        ra_deg: right ascension in degrees.
+        dec_deg: declination in degrees.
+        band: AllWISE band. 
+
+    Returns:
+        a Pandas DataFrame containing valid AllWISE image metadata.
+
+    Raises:
+        SIAQueryFailError: the Simple Image Access Query failed.
+    """
+    try:
+        # NOTE: passing a separate URL params dict to requests.get doesn't work because of the plus signs
+        imglist_url = f"https://irsa.ipac.caltech.edu/SIA?COLLECTION=wise_allwise&POS=circle+{ra_deg:.5f}+{dec_deg:.5f}+0.01&RESPONSEFORMAT=FITS"
+        r = requests.get(imglist_url)
+    except ConnectionError as e:
+        raise SIAQueryFailError(
+            f"Simple Image Access Query failed with message {e.message}!"
+        ) from e
+    t = fits.open(BytesIO(r.content))
+    tab = t[1].data
+    df = Table(tab).to_pandas()
+
+    # Filter by band and science readiness
+    cond = df["energy_bandpassname"] == band
+    cond &= df["dataproduct_subtype"].str.rstrip() == "science"
+
+    # Return DataFrame with valid rows
+    return df.loc[cond]
+
+
 def get_allwise_cutout(
     coords: SkyCoord,
     size: u.Quantity[u.arcmin] = 3 * u.arcmin,
@@ -40,18 +91,19 @@ def get_allwise_cutout(
     save_fits: bool = False,
     cutout_path: Path = None,
 ) -> fits.HDUList | None:
-    """Returns a FITS HDUList of an AllWISE cutout, optionally saving it to file.
+    """Returns a FITS HDUList of an AllWISE cutout, optionally saving to file.
 
     This function extracts a cutout image from AllWISE via the NASA/IPAC
     Infrared Science Archive (IRSA). This is a two-step process: first, a
     Simple Image Access Query is used to identify the list of AllWISE images
-    containing the requested coordinates. The image from which to extract the
-    cutout is taken as the first image in the list in the requested WISE band
-    that is also of "science" quality. Then, a cutout from the image is
-    extracted by constructing a query using the corresponding access URL.
-    If the cutout exists, this function returns an astropy FITS Header Data
-    Unit List (HDUList) containing the FITS header and the image; otherwise an 
-    exception is raised.
+    containing the requested coordinates (see get_allwise_image_list()).  
+    The image from which to extract the cutout is taken as the first image in 
+    the list in the requested WISE band that is also of "science" quality. 
+    Then, a cutout from the image is extracted by constructing a query using 
+    the corresponding access URL. If the cutout exists, this function returns 
+    an astropy FITS Header Data Unit List (HDUList) containing the FITS header 
+    and the image; otherwise an exception is raised.
+
     References:
         https://wise2.ipac.caltech.edu/docs/release/allwise/
         https://irsa.ipac.caltech.edu/ibe/docs/wise/allwise/p3am_cdd/
@@ -78,7 +130,6 @@ def get_allwise_cutout(
     Raises:
         NegativeImageSizeError: the cutout image size is negative.
         InvalidWISEBandError: the specified band is not a valid WISE band.
-        SIAQueryFailError: the Simple Image Access Query failed.
         CutoutNotFoundError: no AllWISE cutout could be found for the input
             combination of coordinates and band.
         CutoutDownloadFailError: an error occurred during the download of the
@@ -107,29 +158,15 @@ def get_allwise_cutout(
         if not cutout_path.suffix:
             cutout_path = cutout_path.with_suffix(".fits")
 
-    # Get list of AllWISE images containing the target RA/Dec, save to a temporary file
-    try:
-        # NOTE: passing a separate URL params dict to requests.get doesn't work because of the plus signs
-        imglist_url = f"https://irsa.ipac.caltech.edu/SIA?COLLECTION=wise_allwise&POS=circle+{ra_deg:.5f}+{dec_deg:.5f}+0.01&RESPONSEFORMAT=FITS"
-        r = requests.get(imglist_url)
-    except ConnectionError as e:
-        raise SIAQueryFailError(
-            f"Simple Image Access Query failed with message {e.message}!"
-        ) from e
-    t = fits.open(BytesIO(r.content))
-    tab = t[1].data
-    df = Table(tab).to_pandas()
-
-    # Filter by band and science readiness
-    cond = df["energy_bandpassname"] == band
-    cond &= df["dataproduct_subtype"].str.rstrip() == "science"
-    if df.loc[cond].shape[0] == 0:
+    # Get list of AllWISE images containing the target RA/Dec
+    df_valid = get_allwise_image_list(ra_deg=ra_deg, dec_deg=dec_deg, band=band)
+    if df_valid.shape[0] == 0:
         raise CutoutNotFoundError(
             f"No valid AllWISE cutouts could be found for inputs RA = {ra_deg:.4f}, dec = {dec_deg:.4f}, band = {band}"
         )
 
     # Get the access URL for the image. If there are multiple then just take the first one
-    access_url = df.loc[cond, "access_url"].values[0].rstrip()
+    access_url = df_valid["access_url"].values[0].rstrip()
 
     # Construct the cutout URL & download
     query_str = f"center={ra_deg:.5f},{dec_deg:.5f}deg&size={size_arcmin:5f}arcmin"
