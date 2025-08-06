bba24dc0fb8d8492f138f734f5b2fe7543014464
MatthewJA <matthew.alger@gmail.com>
2025-08-06 20:14:17 +1000

    Add golden test data generation script.

---
+++ scripts/generate_golden_test_data.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Generates golden test data for RGZ regression tests.
+bazel-bin/rgz/main subjects \
+    --in=data/radio_subjects_test_subset.json \
+    --out=data/radio_subjects_test_subset_processed.json \
+    --cache=data/cache
+bazel-bin/rgz/main classifications \
+    --in=rgz/testdata/radio_classifications_test_subset.json \
+    --out=rgz/testdata/radio_classifications_test_subset_processed.json \
+    --cache=rgz/testdata/first \
+    --subjects=rgz/testdata/radio_subjects_test_subset_processed.json
+bazel-bin/rgz/main host-lookup \
+    --classifications=rgz/testdata/radio_classifications_test_subset_processed.json \
+    --out=rgz/testdata/radio_classifications_test_subset_matched.json
