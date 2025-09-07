392233d64fecd6de897277912cff79c5c4569a8c
Henry Zovaro <henry.zovaro@anu.edu.au>
2025-09-07 16:17:27 +1000

    Test file for plot_classifications

---
+++ rgz/plot_classifications_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+import rgz.plot_classifications
+import rgz.classifications
+
+
+class TestPlotClassifications(unittest.TestCase):
+
+    def test_plot_classifications(self):
+        ...
+
+
+if __name__ == "__main__":
+    unittest.main()
