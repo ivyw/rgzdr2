861d92d88e957a5ddfbb2cde8823103e3476f621
Henry Zovaro <henry.zovaro@anu.edu.au>
2025-10-07 16:52:10 +1100

    Deleted plotting functions

--- rgz/rgz.py
+++ rgz/rgz.py
@@ -10,7 +10,6 @@ from astropy.io import fits
 from astropy.wcs import WCS
 from astropy.coordinates import SkyCoord
 import astropy.wcs
-# import matplotlib.pyplot as plt
 import numpy as np
 import requests
 
@@ -61,79 +60,3 @@ def coord_to_string(coord: SkyCoord) -> HMSDMS:
         # SkyCoord.to_string is not guaranteed to return a string.
         raise TypeError(f"Expected str from SkyCoord.to_string; got {type(coord_str)}")
     return coord_str
-
-
-def plot_contours(
-    raw_subject: JSON,
-    ax=None,
-    bbox_plot_kwargs=None,
-    px_coords=False,
-    px_scaling=100 / constants.RADIO_MAX_PX,
-):
-    """Plots the contours of a raw subject."""
-    # TODO: Fix imports.
-    if not ax:
-        ax = plt.gca()
-    if not bbox_plot_kwargs:
-        bbox_plot_kwargs = {}
-
-    fname = f'first/{raw_subject["_id"]["$oid"]}.json'
-    try:
-        with open(fname) as f:
-            response = json.load(f)
-    except FileNotFoundError:
-        response = requests.get(raw_subject["location"]["contours"]).json()
-        with open(fname, "w") as f:
-            json.dump(response, f)
-    contours = response["contours"]
-    for contour in contours:
-        contour = contour[0]
-        xs = [a["x"] for a in contour["arr"]]
-        ys = [a["y"] for a in contour["arr"]]
-        coords = np.stack([xs, ys]).T
-        if not px_coords:
-            coords = [
-                rgz.subjects.transform_coord_radio(c, raw_subject) for c in coords
-            ]
-            coords = [(a.value, b.value) for a, b in coords]
-        else:
-            coords = [(c[0] * px_scaling, 100 - c[1] * px_scaling) for c in coords]
-        plt.plot(*zip(*coords))
-    #     plt.xlim(plt.xlim()[::-1])
-
-    bboxes = rgz.subjects.get_bboxes(raw_subject)
-    bboxes = [rgz.subjects.transform_bbox(bbox, raw_subject) for bbox in bboxes]
-    for bbox in bboxes:
-        x1, y1, x2, y2 = bbox.value
-        ax.plot([x1, x2, x2, x1, x1], [y1, y1, y2, y2, y1], c="k", **bbox_plot_kwargs)
-
-
-def plot_raw_subject(raw_subject: dict[str, ...], scaling: int = 1):
-    # TODO: Fix imports.
-    f = rgz.subjects.download_first_image(raw_subject)
-    wcs = WCS(f[0].header)
-    ax = plt.subplot(projection=wcs, slices=("x", "y", 0, 0))
-    ax.imshow(f[0].data)
-
-    bboxes = rgz.subjects.get_bboxes(raw_subject)
-    for bbox in bboxes:
-        bbox = np.array(bbox)
-        bbox *= 100 / 132
-        x1, y1, x2, y2 = bbox
-        y2 = 100 - y2
-        y1 = 100 - y1
-        ax.plot([x1, x1, x2, x2, x1], [y2, y1, y1, y2, y2], c="white")
-
-    # transformed bboxes
-    bboxes = [rgz.subjects.transform_bbox(bbox, raw_subject) for bbox in bboxes]
-    for bbox in bboxes:
-        c1 = skcoord.SkyCoord([bbox[:2]], frame="icrs", unit="deg")
-        c2 = skcoord.SkyCoord([bbox[2:]], frame="icrs")
-        bbox = np.array(bbox)
-        x1, y1, x2, y2 = bbox
-        ax.plot(
-            [x1, x1, x2, x2, x1],
-            [y2, y1, y1, y2, y2],
-            c="cyan",
-            transform=ax.get_transform("world"),
-        )
