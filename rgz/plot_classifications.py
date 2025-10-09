3e6ca88eaafa1f6cffc8de82a41504dba8fee582
Henry Zovaro <henry.zovaro@anu.edu.au>
2025-10-09 18:51:42 +1100

    Added docstring; removed TODOs; black formatting

--- rgz/plot_classifications.py
+++ rgz/plot_classifications.py
@@ -1,19 +1,16 @@
 import json
 from pathlib import Path
-import requests
 
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 from astropy.wcs import WCS
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
 
-from rgz import rgz
 from rgz import constants
-from rgz import classifications 
+from rgz import classifications
 from rgz import cutouts
 from rgz import subjects
-
 
 
 def get_contours(
@@ -23,15 +20,14 @@ def get_contours(
     cache: Path = Path("first"),
 ) -> list[tuple]:
     """Returns the contours of a raw subject."""
-    # TODO make this a class method of Subject
-    fname = cache / f'{subject.id}.json'
+    fname = cache / f"{subject.id}.json"
     try:
         with open(fname) as f:
             response = json.load(f)
     except FileNotFoundError as e:
-        raise FileNotFoundError(f"contour data for subject with ID {subject.id}"
-                                f"not found! Have you run download_contours "
-                                "first?")
+        raise FileNotFoundError(
+            f"contour data for subject with ID {subject.id}" f"not found!"
+        )
     contours = response["contours"]
     coords_list = []
     for contour in contours:
@@ -41,10 +37,10 @@ def get_contours(
         coords = np.stack([xs, ys]).T
         if not px_coords:
             coords = [
-                subjects.transform_coord_radio(coord=c, 
-                                               subject=subject, 
-                                               raw_subject=None,
-                                               cache=cache) for c in coords
+                subjects.transform_coord_radio(
+                    coord=c, subject=subject, raw_subject=None, cache=cache
+                )
+                for c in coords
             ]
             coords = [(a.value, b.value) for a, b in coords]
         else:
@@ -53,66 +49,76 @@ def get_contours(
     return coords_list
 
 
-def get_raw_subject(subject: subjects.Subject,
-                    raw_subjects_path: Path,
-                    ) -> rgz.JSON:
-    """Returns the "raw" subject associated with a Subject."""
-    # TODO read directly from FIRST directory instead of taking from the "master" JSON file
-    # the contours are stored in testdata/first/<oid>.json
-    # NOTE: I hate this!!!
-    # NOTE $oid seems to be unique 
-    raw_subjects_jsons = []
-    with open(raw_subjects_path, "r") as f:
-        raw_subjects_rows = f.readlines()
-        for row in raw_subjects_rows:
-            raw_subjects_jsons.append(json.loads(row))
-    raw_subjects_list = [rs for rs in raw_subjects_jsons if rs["_id"]["$oid"] == subject.id]
-    if len(raw_subjects_list) > 1:
-        raise AssertionError(f"I found {len(raw_subjects_list)} raw subjects associated with Subject {subject.id}!")
-    return raw_subjects_list[0]
-
-
 def get_first_coords_from_id(first_id: str) -> SkyCoord:
-    """Returns a SkyCoord object corresponding to FIRST component names."""
-    assert (first_id.startswith("FIRST") or first_id.startswith("NOFIRST"))
+    """Returns a SkyCoord object with coordinates extracted from a FIRST ID."""
+    assert first_id.startswith("FIRST") or first_id.startswith("NOFIRST")
     # TODO should probably use regex for this
     first_coord_nospaces_str = first_id.split("_J")[1]
     sign_str = "+" if "+" in first_coord_nospaces_str else "-"
     ra_nospaces_str, dec_nospaces_str = first_coord_nospaces_str.split(sign_str)
-    ra_str = ra_nospaces_str[:2] + " " + ra_nospaces_str[2:4] + " " + ra_nospaces_str[4:]
-    dec_str = dec_nospaces_str[:2] + " " + dec_nospaces_str[2:4] + " " + dec_nospaces_str[4:]
+    ra_str = (
+        ra_nospaces_str[:2] + " " + ra_nospaces_str[2:4] + " " + ra_nospaces_str[4:]
+    )
+    dec_str = (
+        dec_nospaces_str[:2] + " " + dec_nospaces_str[2:4] + " " + dec_nospaces_str[4:]
+    )
     dec_str = sign_str + dec_str
     first_coords_str = ra_str + " " + dec_str
     return SkyCoord(first_coords_str, unit=(u.hourangle, u.deg))
 
 
 def plot_single_classification(
-        subject: subjects.Subject,
-        raw_subjects_path: Path,
-        cache: Path,
-        classification: classifications.Classification,
-        ax: plt.Axes | None = None) -> plt.Axes:
+    subject: subjects.Subject,
+    classification: classifications.Classification,
+    cache: Path,
+    ax: plt.Axes | None = None,
+) -> plt.Axes:
     """Plot a single classification.
-    
-    NOTE: the FIRST radio images linked in the raw_subjects JSON appear to be 
-    upside down. See here: https://github.com/ivyw/rgzdr2/issues/9#issuecomment-3315480856 
-    """
-    # TODO input checks - check that the subject matches the classification
 
-    # Get the raw subject so we can get the contours
-    # # TODO bypass this entirely 
-    # raw_subject = get_raw_subject(subject=subject, 
-    #                               raw_subjects_path=raw_subjects_path)
+    This function plots the WISE image and radio contours associated with a
+    subject, overlaid with markers indicating the classification made by a
+    citizen scientist. The stretch of the WISE image is intended to be identical
+    to that shown to citizen scientists during the classification process.
+
+    IR host galaxy click locations are represented by crosses, and the
+    corresponding radio sources are indicated with circular markers.
+    Individual host galaxy-radio source associations are indicated by markers
+    of the same colour. If the input classification has
+
+    Args:
+        subject: the subject to plot
+        classification: the classification to plot. The Zooniverse ID must match
+            that of the subject.
+        cache: path to raw subject data.
+        ax: Matplotlib axes (optional). Note that axes passed to this function
+            are first destroyed and re-created with the WCS of the WISE image
+            because there is no way to modify the axis projection after it has
+            been created. The bbox of the original axes is used to create the
+            new axes, so it will remain in the same place in the figure.
+
+    Returns:
+        Matplotlib axes containing the plot.
+
+    Raises:
+        ValueError if the subject Zooniverse ID does not match that of the
+        input classification.
+    """
+    # Check that the subject matches the classification
+    if subject.zid != classification.zid:
+        raise ValueError(
+            f"subject with Zooniverse id {subject.zid} does not "
+            "match that of input classification with id "
+            f"{classification.zid}!"
+        )
 
     # Get the FIRST contours associated with this subject
-    # TODO these don't have any associated labels or FIRST ids...
     contour_coords_list = get_contours(
         subject=subject,
         cache=cache,
         px_coords=False,
     )
 
-    # Get the WISE image associated with this subject 
+    # Get the WISE image associated with this subject
     ra, dec = subject.coords
     coords_wise = SkyCoord(ra, dec, unit="deg")
     hdulist_wise = cutouts.get_allwise_cutout(
@@ -122,83 +128,86 @@ def plot_single_classification(
     )
     wcs_wise = WCS(hdulist_wise[0].header)
 
-
-    # Create figure and axes 
+    # Create figure and axes
     if ax is None:
         fig, ax = plt.subplots(subplot_kw=dict(projection=wcs_wise))
     else:
-        # TODO destroy axis & add a new one with the same bbox with the correct 
-        # projection
+        # Replace existing axes with ones with the correct projection
         fig = ax.get_figure()
+        bbox = ax.get_position()
+        ax.remove()
+        ax = fig.add_axes(rect=bbox, projection=wcs_wise)
 
-    # Plot AllWISE image 
+    ax.set_title(f"Zooniverse ID {subject.zid}\n(subject ID: {subject.id})")
+
+    # Plot AllWISE image. The colour map and limits have been calibrated to
+    # match what was shown to citizen scientists as closely as possible
     ax.imshow(hdulist_wise[0].data, cmap="gist_heat", vmax=6, vmin=2)
 
-    # Plot contours 
-    # TODO can annotate these w/ FIRST component names
+    # Plot contours
+    # TODO annotate these with FIRST IDs, and colour the contours to indicate
+    # the source rather than overplotting a scatter marker.
     contour_colour = "white"
     for contour_coords in contour_coords_list:
-        ax.plot(*zip(*contour_coords), 
-                transform=ax.get_transform("fk5"),
-                color=contour_colour,)
+        ax.plot(
+            *zip(*contour_coords),
+            transform=ax.get_transform("fk5"),
+            color=contour_colour,
+        )
 
-    # Add classifications 
-    # TODO some classifications are "empty" - i.e. coord_matches is []
-    # what do these mean?
-    # TODO does it make sense to plot multiple classifications on the same plot?
-    # esp. since we should probably plot the contours corresponding to different
-    # matches in different colours - e.g. if the subject has 3 radio islands and
-    # a user has associated 2 of them with 1 IR source and the other radio 
-    # island with another IR source
-    # We could group together classifications that have made the SAME associations - 
-    # i.e. show the IR source clicks from all users that have chosen the same 
-    # subset of radio islands. 
-    # TODO can we link first_id below to the contour_coords above?
-    # TODO do we want to make a fancy GUI where you can flick through the 
-    # different classifications for the same subject? 
-    colours = plt.rcParams["axes.prop_cycle"].by_key()['color']
+    # Add classification
+    colours = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
+    # If the coord_matches list is empty, display a warning on the plot and
+    # return.
     if len(classification.coord_matches) == 0:
-        ax.text(s="coord_matches list is empty!",
-                x=0.5, y=0.5, transform=ax.transAxes, va="center", ha="center",
-                color="white",
-                )
-    else:
-        for cc, click in enumerate(classification.coord_matches):
-            coord_str, first_ids = click
-            # Label for this IR source match shown in the legen
-            if coord_str == "NOSOURCE":
-                source_label = "No source"
-            else:
-                source_label = f"Source {cc + 1}"
-            
-            # Plot the locations of the FIRST IDs 
-            for ff, first_id in enumerate(first_ids):
-                first_component_coords = get_first_coords_from_id(first_id)
-                ax.scatter(
-                    first_component_coords.ra.value,
-                    first_component_coords.dec.value,
-                    marker="o",
-                    transform=ax.get_transform("fk5"),
-                    c=colours[cc],
-                    label=f"{source_label} - FIRST component {ff + 1}",
-                )
+        ax.text(
+            s="coord_matches list is empty!",
+            x=0.5,
+            y=0.5,
+            transform=ax.transAxes,
+            va="center",
+            ha="center",
+            color="black",
+        )
+        return ax
 
-            # Plot the location of the IR host
-            if coord_str == "NOSOURCE":
-                continue
-            click_coords = SkyCoord(coord_str, unit=(u.hourangle, u.deg))
-            ax.scatter(click_coords.ra.value, 
-                    click_coords.dec.value,
-                    marker="x",
-                    label=f"{source_label} - IR host",
-                    transform=ax.get_transform("fk5"),
-                    c=colours[cc],
-                    )
-        
-        # Decorations 
-        ax.legend(loc="center left", bbox_to_anchor=[1.05, 0.5])
-    ax.set_title(f"Zooniverse ID {subject.zid} ({subject.id})")
+    # Otherwise, plot each match.
+    for cc, click in enumerate(classification.coord_matches):
+        coord_str, first_ids = click
+        # Label for this IR source match shown in the legend
+        if coord_str == "NOSOURCE":
+            source_label = "No source"
+        else:
+            source_label = f"Source {cc + 1}"
 
+        # Plot the locations of the FIRST IDs
+        for ff, first_id in enumerate(first_ids):
+            first_component_coords = get_first_coords_from_id(first_id)
+            ax.scatter(
+                first_component_coords.ra.value,
+                first_component_coords.dec.value,
+                marker="o",
+                edgecolors="k",
+                transform=ax.get_transform("fk5"),
+                c=colours[cc],
+                label=f"{source_label} - FIRST component {ff + 1}",
+            )
 
-    return ax 
+        # Plot the location of the IR host
+        if coord_str == "NOSOURCE":
+            continue
+        click_coords = SkyCoord(coord_str, unit=(u.hourangle, u.deg))
+        ax.scatter(
+            click_coords.ra.value,
+            click_coords.dec.value,
+            marker="x",
+            label=f"{source_label} - IR host",
+            transform=ax.get_transform("fk5"),
+            c=colours[cc],
+        )
 
+    # Decorations
+    ax.legend(loc="center left", bbox_to_anchor=[1.05, 0.5])
+
+    return ax
