8a81e8ac406d4d1ff46f2c1b72cda10afb00fce2
Henry Zovaro <henry.zovaro@anu.edu.au>
2025-10-21 21:09:04 +1100

    Black formatted

--- rgz/plot_classifications.py
+++ rgz/plot_classifications.py
@@ -21,37 +21,37 @@ def get_contours(
 ) -> list[list[tuple]]:
     """Returns the contours of a subject.
 
-    The raw contour data consists of a series of (x, y) coordinate pairs 
-    relative to the upper-left hand corner of a 132x132 image, where (65, 65) 
-    represents the centre of the image. If px_coords is True, this function 
-    applies applies a stretch defined by the px_scaling arg such that 
+    The raw contour data consists of a series of (x, y) coordinate pairs
+    relative to the upper-left hand corner of a 132x132 image, where (65, 65)
+    represents the centre of the image. If px_coords is True, this function
+    applies applies a stretch defined by the px_scaling arg such that
     the coordinates are defined on a
 
          (px_scaling * RADIO_MAX_PX) x (px_scaling * RADIO_MAX_PX)
-    
-    grid. By default, px_scaling is set so that the returned coordinates are 
+
+    grid. By default, px_scaling is set so that the returned coordinates are
     defined on a 100 x 100 grid to match the dimensions of the FIRST images.
 
-    Note this function expects the contour data to be located in 
-    cache / f"{subject.id}.json". An exception is raised if the data cannot be 
+    Note this function expects the contour data to be located in
+    cache / f"{subject.id}.json". An exception is raised if the data cannot be
     found.
-    
+
     Args:
         subject: the subject.
-        px_coords: if True, contour coordinates are returned in pixel units 
-            relative to the upper left-hand corner of the image. If False, 
+        px_coords: if True, contour coordinates are returned in pixel units
+            relative to the upper left-hand corner of the image. If False,
             they are given in degrees as RA/dec pairs.
         px_scaling: Stretch applied to contour coordinates if px_coords is True.
             Ignored if px_coords is False.
         cache: path to contour data.
 
     Returns:
-        A list of lists each representing a radio island, each of which 
+        A list of lists each representing a radio island, each of which
         contains a list of (x, y) pairs for each contour.
 
     Raises:
-        FileNotFoundError if the file containing the contour data cannot be 
-        found. 
+        FileNotFoundError if the file containing the contour data cannot be
+        found.
 
     """
     fname = cache / f"{subject.id}.json"
@@ -88,6 +88,7 @@ def get_first_coords_from_id(first_id: str) -> SkyCoord:
     """Returns a SkyCoord object with coordinates extracted from a FIRST ID."""
     assert first_id.startswith("FIRST") or first_id.startswith("NOFIRST")
     # TODO(hzovaro) should probably use regex for this
+    # TODO(hzovaro) and write some simple tests
     first_coord_nospaces_str = first_id.split("_J")[1]
     sign_str = "+" if "+" in first_coord_nospaces_str else "-"
     ra_nospaces_str, dec_nospaces_str = first_coord_nospaces_str.split(sign_str)
