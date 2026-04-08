693802085b6c473ef28925fcf1a0e575377077a8
Henry Zovaro <henry.zovaro@anu.edu.au>
2026-04-08 21:18:19 +1000

    Added phys_bbox_list and bbox_list class variables to Subject

--- rgz/subjects.py
+++ rgz/subjects.py
@@ -56,7 +56,9 @@ class Subject:
     id: str = attr.ib()
     zid: ZooniverseID = attr.ib()
     coords: tuple[float, float] = attr.ib()
-    bboxes: dict[BBox, list[FIRSTID]] = attr.ib()
+    bbox_list: list[BBox] = attr.ib()
+    phys_bbox_list: list[BBox] = attr.ib()
+    bboxes: dict[BBox, list[FIRSTID]] = attr.ib()  # TODO consider renaming
 
     def to_json(self) -> rgz.JSON:
         """Converts a Subject into a JSON-compatible dictionary."""
@@ -64,6 +66,8 @@ class Subject:
             "id": self.id,
             "zid": self.zid,
             "coords": self.coords,
+            "bbox_list": self.bbox_list,
+            "phys_bbox_list": [[c.value for c in bbox] for bbox in self.phys_bbox_list],
             "bboxes": [{"bbox": list(k), "first": v} for k, v in self.bboxes.items()],
         }
 
@@ -74,6 +78,8 @@ class Subject:
             subject["id"],
             subject["zid"],
             subject["coords"],
+            subject["bbox_list"],
+            subject["phys_bbox_list"],
             {tuple(b["bbox"]): b["first"] for b in subject["bboxes"]},
         )
 
@@ -339,11 +345,25 @@ def process_subject(
     sid = raw_subject["_id"]["$oid"]
     zid = raw_subject["zooniverse_id"]
     bboxes = get_bboxes(raw_subject, cache)
+
+    # Get bboxes in physical coordinates 
+    phys_bboxes = [
+        transform_bbox_px_to_phys(bbox, raw_subject, cache) for bbox in bboxes
+    ]
+
+    # Construct dict mapping bboxes to FIRST IDs
     bbox_to_firsts = {}
     for bbox in bboxes:
         firsts = get_first_from_bbox(bbox, raw_subject, cache, first_tree)
         bbox_to_firsts[bbox] = firsts
-    s = Subject(id=sid, zid=zid, coords=raw_subject["coords"], bboxes=bbox_to_firsts)
+
+    # Create class instance 
+    s = Subject(id=sid, 
+                zid=zid, 
+                coords=raw_subject["coords"], 
+                bbox_list=bboxes,
+                phys_bbox_list=phys_bboxes,
+                bboxes=bbox_to_firsts)
     return s
 
 
