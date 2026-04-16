0dd75f29ae7bf2ac9951757295aa42c43b7818f4
Henry Zovaro <henry.zovaro@anu.edu.au>
2026-04-16 22:04:07 +1000

    Stated implementing test for transform_bbox_px_to_phys

--- rgz/subjects_test.py
+++ rgz/subjects_test.py
@@ -27,6 +27,16 @@ _TEST_SUBJECTS_PATH = _TEST_DIR / "subjects.json"
 _TEST_SUBJECTS_PROCESSED_PATH = _TEST_DIR / "subjects_processed.json"
 
 
+def make_dummy_wcs():
+    # Make a dummy WCS 
+    w = WCS(naxis=2)
+    w.wcs.cdelt = [0.3, 0.3]
+    w.wcs.crpix = [10, 15]
+    w.wcs.crval = [2, 3]
+    return w
+    
+
+
 class TestFindPointsInBox(unittest.TestCase):
     """Tests for rgz.subjects.find_points_in_box."""
 
@@ -100,14 +110,7 @@ class TestProcess(unittest.TestCase):
 class TestTransformCoordRadio(unittest.TestCase):
     """Tests for transform_coord_radio."""
 
-    def make_dummy_wcs(self):
-        # Make a dummy WCS 
-        w = WCS(naxis=2)
-        w.wcs.cdelt = [0.3, 0.3]
-        w.wcs.crpix = [10, 15]
-        w.wcs.crval = [2, 3]
-        return w
-    
+
     
     def test_all_pix2world(self):
         """Sanity check for WCS.all_pix2world."""
@@ -119,7 +122,7 @@ class TestTransformCoordRadio(unittest.TestCase):
         ])
         # NOTE: w.wcs.crpix is indexed from 1, so we have to subtract 1 as 
         # below.
-        w = self.make_dummy_wcs()
+        w = make_dummy_wcs()
         coords_px = (
             (coords_phys - w.wcs.crval) / w.wcs.cdelt
             + (w.wcs.crpix - 1)
@@ -132,7 +135,7 @@ class TestTransformCoordRadio(unittest.TestCase):
 
 
     def test_transform_coord_radio(self):
-        w = self.make_dummy_wcs() 
+        w = make_dummy_wcs() 
 
         # Check for coords being out of bounds
         with self.assertRaises(ValueError):
@@ -144,7 +147,7 @@ class TestTransformCoordRadio(unittest.TestCase):
         for coords in [[0, 0], [1, 10], [99, 100], [131, 5]]:
             coords_unscaled_px = np.array([coords])
             coords_px = (
-                coords_unscaled_px / 132 * 100  # rgz.constants.RADIO_MAX_PX * 100
+                coords_unscaled_px / rgz.constants.RADIO_MAX_PX * 100
             )
             coords_phys = (coords_px - (w.wcs.crpix - 1)) * w.wcs.cdelt + w.wcs.crval
             want = coords_phys
@@ -157,10 +160,29 @@ class TestTransformCoordRadio(unittest.TestCase):
 
 class TestTransformBboxPxToPhys(unittest.TestCase):
     """Tests for transform_bbox_px_to_phys."""
+    # TODO(hzovaro): update once bboxes are refactored
 
-    def test_transformation(self):
-        # Make a dummy WCS 
-        ...
+    def test_transform(self):
+        # TODO(hzovaro) test some more coords
+        xmin = 1
+        ymin = 1
+        xmax = 2
+        ymax = 2
+        bbox = [
+            xmin, ymin, xmax, ymax 
+        ]
+        w = make_dummy_wcs()
+        want = np.concatenate(
+            [
+                rgz.subjects.transform_coord_radio(coord=np.array([xmin, 132 - ymax]), wcs=w),
+                rgz.subjects.transform_coord_radio(coord=np.array([xmax, 132 - ymin]), wcs=w)
+            ]
+        )
+        got = rgz.subjects.transform_bbox_px_to_phys(
+            px_bbox=bbox,
+            wcs=w
+        )
+        self.assertTrue(np.allclose(want, got))
 
 
 
