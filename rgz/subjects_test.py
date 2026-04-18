bdb10382549a26ab636f1353486228e05af4a513
Henry Zovaro <henry.zovaro@anu.edu.au>
2026-04-18 12:03:40 +1000

    Black formatted

--- rgz/subjects_test.py
+++ rgz/subjects_test.py
@@ -28,13 +28,12 @@ _TEST_SUBJECTS_PROCESSED_PATH = _TEST_DIR / "subjects_processed.json"
 
 
 def make_dummy_wcs():
-    # Make a dummy WCS 
+    # Make a dummy WCS
     w = WCS(naxis=2)
     w.wcs.cdelt = [0.3, 0.3]
     w.wcs.crpix = [10, 15]
     w.wcs.crval = [2, 3]
     return w
-    
 
 
 class TestFindPointsInBox(unittest.TestCase):
@@ -104,38 +103,37 @@ class TestProcess(unittest.TestCase):
 
 
 # TODO(hzovaro): add test for serialisation/deserialisation - in particular that
-# transforming a WCS from WCS object -> string -> object doesn't result in any 
-# problematic differences 
+# transforming a WCS from WCS object -> string -> object doesn't result in any
+# problematic differences
 # TODO(hzovaro): add test for transform_bbox_px_to_phys
 class TestTransformCoordRadio(unittest.TestCase):
     """Tests for transform_coord_radio."""
 
-
-    
     def test_all_pix2world(self):
         """Sanity check for WCS.all_pix2world."""
-        # Create some dummy RA/Dec pairs and check that the common-sense 
+        # Create some dummy RA/Dec pairs and check that the common-sense
         # transformation gives the same result as w.all_pix2world
-        coords_phys = np.array([
-            [1, 5], [-1, 10], [99, 100], [256, .3],
-            [-99, 0.0003], [359., 179.], [1.3454, 63.0324],
-        ])
-        # NOTE: w.wcs.crpix is indexed from 1, so we have to subtract 1 as 
+        coords_phys = np.array(
+            [
+                [1, 5],
+                [-1, 10],
+                [99, 100],
+                [256, 0.3],
+                [-99, 0.0003],
+                [359.0, 179.0],
+                [1.3454, 63.0324],
+            ]
+        )
+        # NOTE: w.wcs.crpix is indexed from 1, so we have to subtract 1 as
         # below.
         w = make_dummy_wcs()
-        coords_px = (
-            (coords_phys - w.wcs.crval) / w.wcs.cdelt
-            + (w.wcs.crpix - 1)
-        )
+        coords_px = (coords_phys - w.wcs.crval) / w.wcs.cdelt + (w.wcs.crpix - 1)
         want = coords_phys
-        got = w.all_pix2world(coords_px, 0) 
-        self.assertTrue(
-            np.allclose(want, got)
-        )
-
+        got = w.all_pix2world(coords_px, 0)
+        self.assertTrue(np.allclose(want, got))
 
     def test_transform_coord_radio(self):
-        w = make_dummy_wcs() 
+        w = make_dummy_wcs()
 
         # Check for coords being out of bounds
         with self.assertRaises(ValueError):
@@ -146,20 +144,16 @@ class TestTransformCoordRadio(unittest.TestCase):
         # Check transformation has worked correctly
         for coords in [[0, 0], [1, 10], [99, 100], [131, 5]]:
             coords_unscaled_px = np.array([coords])
-            coords_px = (
-                coords_unscaled_px / rgz.constants.RADIO_MAX_PX * 100
-            )
+            coords_px = coords_unscaled_px / rgz.constants.RADIO_MAX_PX * 100
             coords_phys = (coords_px - (w.wcs.crpix - 1)) * w.wcs.cdelt + w.wcs.crval
             want = coords_phys
-            got = rgz.subjects.transform_coord_radio(coords_unscaled_px[0],
-                                                     wcs=w)
-            self.assertTrue(np.allclose(
-                want, np.array([[c.value for c in got]]))
-            )
+            got = rgz.subjects.transform_coord_radio(coords_unscaled_px[0], wcs=w)
+            self.assertTrue(np.allclose(want, np.array([[c.value for c in got]])))
 
 
 class TestTransformBboxPxToPhys(unittest.TestCase):
     """Tests for transform_bbox_px_to_phys."""
+
     # TODO(hzovaro): update once bboxes are refactored
 
     def test_transform(self):
@@ -168,22 +162,20 @@ class TestTransformBboxPxToPhys(unittest.TestCase):
         ymin = 1
         xmax = 2
         ymax = 2
-        bbox = [
-            xmin, ymin, xmax, ymax 
-        ]
+        bbox = [xmin, ymin, xmax, ymax]
         w = make_dummy_wcs()
         want = np.concatenate(
             [
-                rgz.subjects.transform_coord_radio(coord=np.array([xmin, 132 - ymax]), wcs=w),
-                rgz.subjects.transform_coord_radio(coord=np.array([xmax, 132 - ymin]), wcs=w)
+                rgz.subjects.transform_coord_radio(
+                    coord=np.array([xmin, 132 - ymax]), wcs=w
+                ),
+                rgz.subjects.transform_coord_radio(
+                    coord=np.array([xmax, 132 - ymin]), wcs=w
+                ),
             ]
         )
-        got = rgz.subjects.transform_bbox_px_to_phys(
-            px_bbox=bbox,
-            wcs=w
-        )
+        got = rgz.subjects.transform_bbox_px_to_phys(px_bbox=bbox, wcs=w)
         self.assertTrue(np.allclose(want, got))
-
 
 
 if __name__ == "__main__":
