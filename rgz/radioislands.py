13ca1699a518e34b9c4dae191c0aab96b923dcd5
Henry Zovaro <henry.zovaro@anu.edu.au>
2026-05-09 12:02:07 +1000

    Moved functions from subjects

--- rgz/radioislands.py
+++ rgz/radioislands.py
@@ -48,20 +48,10 @@ type FIRSTTree = tuple[npt.NDArray[np.float64], list[str]]
     requests.exceptions.ConnectionError,
     max_tries=MAX_TRIES,
 )
-def download_first(coord: SkyCoord, image_size: Quantity[u.arcmin]) -> fits.HDUList:
-    """Downloads a FIRST image from the FIRST server."""
-    ims = First.get_images(coord, image_size=image_size)
-    if isinstance(ims, fits.HDUList):
-        return ims
-    # Technically allowed by documentation, but I don't expect it to happen
-    # with the files we're opening (i.e. FIRST survey files).
-    raise TypeError(f"Expected HDUList; got {type(ims)}")
-
-
-def fetch_first_image_from_server_or_cache(
+def download_first_image(
     raw_subject: rgz.JSON | None,
     cache: Path,
-) -> fits.HDUList:
+) -> None:
     """Fetches a FIRST image from the FIRST server or cache given a subject.
     The subject can be specified either via a raw subject (in JSON format) or a
     processed subject (as a Subject object).
@@ -94,13 +84,39 @@ def fetch_first_image_from_server_or_cache(
     ra, dec = raw_subject["coords"]
     coord = SkyCoord(ra=ra, dec=dec, unit="deg")
     fname = cache / f'{raw_subject["_id"]["$oid"]}.fits'
-    try:
-        return fits.open(fname)
-    except FileNotFoundError:
-        logger.debug("Cache miss; downloading %s", fname)
-        im = download_first(coord, image_size=3 * u.arcmin)
-        im.writeto(fname)
-        return im
+    if Path(fname).exists():
+        logger.warn(f"File {fname} already exists! Not re-downloading")
+        return
+    logger.debug("Cache miss; downloading %s", fname)
+    # Previously:
+    # im = download_first(coord, image_size=3 * u.arcmin)
+    ims = First.get_images(coord, image_size=3 * u.arcmin)
+    if not isinstance(ims, fits.HDUList):
+        # Technically allowed by documentation, but I don't expect it to happen
+        # with the files we're opening (i.e. FIRST survey files).
+        raise TypeError(f"Expected HDUList; got {type(ims)}")
+    ims.writeto(fname)
+
+
+def load_first_image(
+    sid: str,
+    cache: Path,
+) -> fits.HDUList:
+    """Fetches FIRST image data for a subject.
+    #TODO(hzovaro): what is the appropriate return type?
+    """
+    # TODO(hzovaro): this should be a method of radioisland
+    fname = cache / f'{sid}.fits'
+    return fits.open(fname)
+
+
+def get_wcs(
+    sid: str,
+    cache: Path,
+) -> WCS:
+    """Fetches the WCS of a subject."""
+    hdulist = load_first_image(sid, cache)
+    return rgz.get_wcs(hdulist)
 
 
 def fetch_first_catalogue_from_server_or_cache(
@@ -138,6 +154,7 @@ def download_first_catalogue(cache: Path):
     first[0]["RA_DEG"] = ra
     first[0]["DE_DEG"] = dec
     first[0].write(cache / constants.FIRST_CATALOGUE_NAME, format="csv")
+
 
 def get_first_from_bbox(
     px_bbox: BBox,
@@ -251,6 +268,117 @@ def find_points_in_box(
     return list(mask.nonzero()[0])
 
 
+def download_contour_data(
+    raw_subject: rgz.JSON,
+    cache: Path,
+):
+    """Fetches contour data for a raw subject, caching locally.
+    #TODO(hzovaro): what is the appropriate return type?
+    """
+    # NOTE: this function is what creates the file below.
+    # TODO(hzovaro): should this be a method of radioisland?
+    # TODO(hzovaro): handle file already exists properly
+    fname = cache / f'{raw_subject["_id"]["$oid"]}.json'
+    if Path(fname).exists():
+        logger.warn(f"File {fname} already exists! Not re-downloading")
+        return
+    url = raw_subject["location"]["contours"]
+    response = requests.get(url)
+    if not response.ok:
+        if response.status_code == 404:
+            raise FileNotFoundError(f"HTTP 404: {url}")
+        raise RuntimeError("Error:", response.status_code)
+    js = response.json()
+    assert abs(js["width"] - constants.RADIO_MAX_PX) <= 1
+    with open(fname, "w") as f:
+        # Don't indent here to keep the filesize down.
+        # These don't need to be human-readable.
+        json.dump(js, f)
+
+
+def load_contour_data(
+    sid: str,
+    cache: Path,
+):
+    """Fetches contour data for a raw subject.
+    #TODO(hzovaro): what is the appropriate return type?
+    """
+    # TODO(hzovaro): this should be a method of radioisland
+    fname = cache / f'{sid}.json'
+    with open(fname) as f:
+        return json.load(f)        
+
+
+def get_bboxes(
+    sid: str,
+    cache: Path,
+) -> Sequence[BBox]:
+    """Fetches the bboxes of a subject."""
+    js = load_contour_data(sid, cache)
+    bboxes = []
+    for contour in js["contours"]:
+        assert contour[0]["k"] == 0
+        # Bboxes are 1-indexed...
+        bboxes.append(tuple([round(c, 1) - 1 for c in contour[0]["bbox"]]))
+    return tuple(bboxes)
+
+
+class ContoursNotFoundError(Exception):
+    """Raised when the contours list in the raw subject JSON file is empty."""
+
+
+def get_contours(
+    sid: str,
+    cache: Path,
+) -> list[list[tuple]]:
+    """Returns the contours of a subject.
+
+    # TODO(hzovaro): return type? 
+    # TODO(hzovaro): what about contour level?
+    # TODO(hzovaro): make sure the coordinate system is consistent with whatever 
+    get_bboxes returns. Also, rounding.
+
+    The raw contour data consists of a series of (x, y) coordinate pairs
+    relative to the upper-left hand corner of a 132x132 image, where (65, 65)
+    represents the centre of the image. If px_coords is True, this function
+    applies applies a stretch defined by the px_scaling arg such that
+    the coordinates are defined on a
+
+         (px_scaling * RADIO_MAX_PX) x (px_scaling * RADIO_MAX_PX)
+
+    grid. By default, px_scaling is set so that the returned coordinates are
+    defined on a 100 x 100 grid to match the dimensions of the FIRST images.
+
+    Args:
+        subject: the subject.
+        cache: path to contour data.
+
+    Returns:
+        A list of lists each representing a radio island, each of which
+        contains a list of (x, y) pairs for each contour.
+
+    Raises:
+        FileNotFoundError if the file containing the contour data cannot be
+        found.
+        
+    """
+    js = load_contour_data(sid, cache)
+    px_scaling = 100 / constants.RADIO_MAX_PX
+    island_contours = []
+    for island in js["contours"]:
+        contours = []
+        for contour in island:
+            xs = [coord["x"] for coord in contour["arr"]]
+            ys = [constants.RADIO_MAX_PX - 1 - coord["y"] for coord in contour["arr"]]
+            coords = np.stack([xs, ys]).T
+            coords = [(x * px_scaling, y * px_scaling) for x, y in coords]
+            contours.append(coords)
+        island_contours.append(contours)
+
+    if len(island_contours) == 0:
+        # TODO(hzovaro): should probs quote the subject ID here 
+        raise ContoursNotFoundError(f"Contour data not found!")
+    return island_contours
 
 
 # @dataclass
