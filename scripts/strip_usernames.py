fa98dc3861ee561622e215988e00d6b7f61b9343
MatthewJA <matthew.alger@gmail.com>
2026-05-10 19:41:52 +1000

    Add username strip script

---
+++ scripts/strip_usernames.py
@@ -0,0 +1,19 @@
+import json
+
+import click
+
+@click.command()
+@click.argument("in_")
+@click.argument("out")
+def main(in_, out):
+    with open(in_) as f:
+        j = json.load(f)
+    for i, c in enumerate(j):
+        if 'username' in c:
+            j[i] = {k: v for k, v in c.items() if k != 'username'}
+    with open(out, 'w') as f:
+        json.dump(j, f)
+
+
+if __name__ == '__main__':
+    main()
