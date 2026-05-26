d40aaae661144b7141257262724543d86c26ef51
Henry Zovaro <henry.zovaro@anu.edu.au>
2026-05-26 21:26:59 +1000

    Scripts for debugging - may not work

---
+++ scripts/debug_contours.py
@@ -0,0 +1,54 @@
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
+from pathlib import Path
+
+from rgz import subjects
+from rgz import radioislands
+from rgz import constants
+from rgz import classifications
+from rgz.plot import classifications as plot_classifications
+from rgz.plot import subject as plot_subject
+
+import matplotlib
+import matplotlib.pyplot as plt
+plt.ion()
+plt.close("all")
+
+# Paths 
+cache_path = Path("data") / "cache"
+testdata_path = Path("rgz") / "testdata"
+raw_subjects_path = testdata_path / "subjects.json"
+processed_subjects_path = testdata_path / "subjects_processed.json"
+processed_classifications_path = testdata_path / "classifications_processed.json"
+
+
+with open(processed_subjects_path, "r") as f:
+    subject_instances = [subjects.Subject.from_json(s) for s in json.load(f)]
+
+# Get bboxes and contours and plot them.
+s = subject_instances[1]
+
+# xmin, ymin, xmax, ymax
+bboxes = radioislands.get_bboxes(s.id, cache=cache_path)
+contours = radioislands.get_contours(s.id, cache=cache_path)
+
+# Plot 
+fig, ax = plt.subplots()
+contour_colour = "white"
+for contour in contours[0]:
+    ax.plot(
+        *zip(*contour),
+        color=contour_colour,
+        path_effects=[
+            matplotlib.patheffects.withStroke(linewidth=2, foreground="red")
+        ],
+    )
+for bbox in bboxes:
+    xmin, ymin, xmax, ymax = bbox
+    ax.plot(
+        [xmin, xmax, xmax, xmin, xmin],
+        [ymin, ymin, ymax, ymax, ymin],
+        color="k"
+    )
