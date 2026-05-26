d3728315d2ac107790b0153c0e3b0edba613624f
Henry Zovaro <henry.zovaro@anu.edu.au>
2026-05-26 21:23:04 +1000

    Implemented tests for new BBox and RadioIsland classes

--- rgz/radioislands_test.py
+++ rgz/radioislands_test.py
@@ -90,26 +90,19 @@ class TestFindPointsInBox(unittest.TestCase):
 
 
 class TestTransformBboxPxToPhys(unittest.TestCase):
-    """Tests for transform_bbox_px_to_phys."""
+    """Tests for transform_rgzbbox_to_phys."""
 
-    # TODO(hzovaro): update once bboxes are refactored
-
+    @unittest.skip("transform_coord_radio has been removed")
     def test_transform(self):
-        # TODO(hzovaro) test some more coords
+        # TODO(hzovaro) Replace transform_coord_radio 
         xmin = 1
         ymin = 2
         xmax = 2
         ymax = 1
         bbox = (xmin, ymin, xmax, ymax)
-
         w = make_dummy_wcs()
-        # TODO this test needs to be updated since transform_coord_radio has been removed.
         want = np.concatenate(
             [
-                # NOTE: transform_coord_radio assumes input args have origin of
-                # 1 but have been flipped
-                # NOTE 2: in the before code, the order below was ymax on the
-                # top line and ymin on the bottom. But this doesn't make sense.
                 rgz.radioislands.transform_coord_radio(
                     coord=np.array([xmin - 1, 131 - (ymin - 1)]),
                     wcs=w,  # = 131 - 1 = 130
@@ -127,7 +120,7 @@ class TestTransformBboxPxToPhys(unittest.TestCase):
             ymax=want[3],
         )
 
-        got = rgz.radioislands.transform_bbox_px_to_phys(bbox=bbox, wcs=w)
+        got = rgz.radioislands.transform_rgzbbox_to_phys(bbox=bbox, wcs=w)
         self.assertEqual(want, got)
 
 
@@ -180,7 +173,7 @@ class TestBBox(unittest.TestCase):
             ra_min=ra_min,
             ra_max=ra_max,
             dec_min=dec_min,
-            dec_max=dec_max,
+             dec_max=dec_max,
             width=ra_max - ra_min,
             height=dec_max - dec_min,
             centre=[
@@ -191,13 +184,121 @@ class TestBBox(unittest.TestCase):
 
         # Test serialisation
         want = bbox_dict
-        got = bbox.to_dict()
+        got = bbox.to_json()
         self.assertDictEqual(want, got)
 
         # Test deserialisation
         want = bbox
-        got = rgz.radioislands.BBox.from_dict(bbox_dict)
+        got = rgz.radioislands.BBox.from_json(bbox_dict)
         self.assertEqual(want, got)
+
+class TestRadioIsland(unittest.TestCase):
+
+    def test_inputs(self):
+        """Tests for input validation."""
+        # TODO(hzovaro): implement these
+
+    def test_eq(self):
+        """Test the __eq__ method."""
+        rng = np.random.RandomState(42)
+
+        ra_min_1 = 10
+        dec_min_1 = 5
+        ra_max_1 = 12
+        dec_max_1 = 8
+        bbox_1 = rgz.radioislands.BBox(
+            ra_min=ra_min_1 * u.deg,
+            ra_max=ra_max_1 * u.deg,
+            dec_min=dec_min_1 * u.deg,
+            dec_max=dec_max_1 * u.deg,
+        )
+        rgzbbox_1 = [1, 2, 3, 4]
+        firsts_1 = ["FIRST1", "FIRST2", "FIRST3"]
+        contours_1 = [
+            [(ra, dec) for ra, dec in zip(
+                rng.uniform(low=50, high=51, size=25),
+                rng.uniform(low=-45, high=-44, size=25))],
+            [(ra, dec) for ra, dec in zip(
+                rng.uniform(low=50, high=51, size=25),
+                rng.uniform(low=-45, high=-44, size=25))],
+        ]
+
+        ri_1 = rgz.radioislands.RadioIsland(
+            bbox=bbox_1, rgzbbox=rgzbbox_1, contours=contours_1, firsts=firsts_1
+        )
+        
+        ra_min_2 = 25
+        dec_min_2 = 16
+        ra_max_2 = 33.5
+        dec_max_2 = 25
+        bbox_2 = rgz.radioislands.BBox(
+            ra_min=ra_min_2 * u.deg,
+            ra_max=ra_max_2 * u.deg,
+            dec_min=dec_min_2 * u.deg,
+            dec_max=dec_max_2 * u.deg,
+        )
+        rgzbbox_2 = [1, 2, 3, 4]
+        firsts_2 = ["FIRST1", "FIRST2", None]
+        contours_2 = [
+            [(0.1, 1.3), (0.2, 1.3), (0.05, 0.9), (0.5, 1.5), (0.1, np.nan),],
+            [(0.1, np.nan), (0.2, 1.3), (0.05, 0.9), (0.5, 1.5), (0.1, np.inf),],
+        ]
+
+        ri_2 = rgz.radioislands.RadioIsland(
+            bbox=bbox_2, rgzbbox=rgzbbox_2, contours=contours_2, firsts=firsts_2
+        )
+
+        self.assertTrue(ri_1 == ri_1)
+        self.assertTrue(ri_2 == ri_2)
+        self.assertFalse(ri_1 == ri_2)
+
+
+    def test_serialisation(self):
+        """Tests for the to_json() and from_json() methods."""
+        rng = np.random.RandomState(42)
+        ra_min = 10
+        dec_min = 5
+        ra_max = 12
+        dec_max = 8
+        bbox = rgz.radioislands.BBox(
+            ra_min=ra_min * u.deg,
+            ra_max=ra_max * u.deg,
+            dec_min=dec_min * u.deg,
+            dec_max=dec_max * u.deg,
+        )
+        rgzbbox = [1, 2, 3, 4]
+        firsts = ["FIRST1", "FIRST2", "FIRST3"]
+        contours = [
+            [(ra, dec) for ra, dec in zip(
+                rng.uniform(low=50, high=51, size=25),
+                rng.uniform(low=-45, high=-44, size=25))],
+            [(ra, dec) for ra, dec in zip(
+                rng.uniform(low=50, high=51, size=25),
+                rng.uniform(low=-45, high=-44, size=25))],
+        ]
+
+        ri = rgz.radioislands.RadioIsland(
+            bbox=bbox, rgzbbox=rgzbbox, contours=contours, firsts=firsts
+        )
+        ri_dict = dict(
+            bbox=bbox.to_json(),
+            rgzbbox=rgzbbox,
+            contours=contours,
+            firsts=firsts,
+        )
+
+        # Serialisation
+        want = ri_dict 
+        got = ri.to_json()
+        self.assertEqual(want, got)
+        
+        # Deserialisation
+        want = ri 
+        got = rgz.radioislands.RadioIsland.from_json(ri_dict)
+        self.assertEqual(want.bbox, got.bbox)
+        self.assertEqual(want.rgzbbox, got.rgzbbox)
+        self.assertEqual(want.firsts, got.firsts)
+        self.assertEqual(want.contours, got.contours)
 
 
 if __name__ == "__main__":
