d809b35770845c70a86621ef796cc0f7c21ac94d
Henry Zovaro <henry.zovaro@anu.edu.au>
2026-05-27 19:18:34 +1000

    Updated script for debugging

--- scripts/debug_contours.py
+++ scripts/debug_contours.py
@@ -11,6 +11,7 @@ from rgz import classifications
 from rgz.plot import classifications as plot_classifications
 from rgz.plot import subject as plot_subject
 
+from astropy.wcs import WCS
 import matplotlib
 import matplotlib.pyplot as plt
 plt.ion()
@@ -25,26 +26,30 @@ processed_classifications_path = testdata_path / "classifications_processed.json
 
 
 with open(processed_subjects_path, "r") as f:
-    subject_instances = [subjects.Subject.from_json(s) for s in json.load(f)]
+    subject_instances = json.load(f)
 
 # Get bboxes and contours and plot them.
-s = subject_instances[1]
+zid = "ARG00031go"
+s = [s for s in subject_instances if s["zid"] == zid][0]
+id = s["id"]
+wcs = WCS(s["wcs"])
 
 # xmin, ymin, xmax, ymax
-bboxes = radioislands.get_bboxes(s.id, cache=cache_path)
-contours = radioislands.get_contours(s.id, cache=cache_path)
+bboxes = radioislands.get_bboxes(id, wcs=wcs, cache=cache_path)
+contours = radioislands.get_contours(id, wcs=wcs, cache=cache_path)
 
 # Plot 
 fig, ax = plt.subplots()
 contour_colour = "white"
-for contour in contours[0]:
-    ax.plot(
-        *zip(*contour),
-        color=contour_colour,
-        path_effects=[
-            matplotlib.patheffects.withStroke(linewidth=2, foreground="red")
-        ],
-    )
+for island in contours:
+    for contour in island:
+        ax.plot(
+            *zip(*contour),
+            color=contour_colour,
+            path_effects=[
+                matplotlib.patheffects.withStroke(linewidth=2, foreground="red")
+            ],
+        )
 for bbox in bboxes:
     xmin, ymin, xmax, ymax = bbox
     ax.plot(
