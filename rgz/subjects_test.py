ae801f42bcee7b0b437109c0f970c74e13f634b9
MatthewJA <matthew.alger@gmail.com>
2026-07-03 18:53:02 +1000

    Fixed float comparisons for regression test

--- rgz/subjects_test.py
+++ rgz/subjects_test.py
@@ -11,6 +11,7 @@ import unittest
 import rgz.consensus
 import rgz.constants
 import rgz.subjects
+from rgz import testutils
 
 logger = logging.getLogger(__name__)
 
@@ -52,60 +53,7 @@ class TestProcess(unittest.TestCase):
         with open(_TEST_SUBJECTS_PROCESSED_PATH) as f:
             want = json.load(f)
 
-        self.assertEqual(want, got)
-        return
-
-        subject_ids_old = [s["id"] for s in want]
-        subject_ids_new = [s["id"] for s in got]
-        self.assertEqual(set(subject_ids_new), set(subject_ids_old))
-
-        # Sort
-        want = [want[ii] for ii in np.argsort(subject_ids_old)]
-        got = [got[ii] for ii in np.argsort(subject_ids_new)]
-
-        # Check len is the same
-        self.assertTrue(len(want), len(got))
-
-        # Check attributes are the same
-        ii = 0
-        for subject_old, subject_new in zip(want, got):
-            self.assertEqual(subject_old["id"], subject_new["id"])
-            self.assertEqual(subject_old["zid"], subject_new["zid"])
-            self.assertEqual(subject_old["coords"], subject_new["coords"])
-            self.assertEqual(subject_old["wcs"], subject_new["wcs"])
-
-            # Check same number of radio_islands
-            breakpoint()
-            if not (len(subject_old["bboxes"]) == len(subject_new["radio_islands"])):
-                breakpoint()
-            self.assertEqual(
-                len(subject_old["bboxes"]), len(subject_new["radio_islands"])
-            )
-
-            # Check firsts are the same
-            for bbox_old, ri_new in zip(
-                subject_old["bboxes"],
-                subject_new["radio_islands"],
-            ):
-                self.assertEqual(
-                    bbox_old["bbox"],
-                    [
-                        c - 1 for c in ri_new["rgzbbox"]
-                    ],  # NOTE: need to subtract 1 from these since in the code that produced the test data these coords had already been shifted so that the origin was (0, 0).
-                )
-                # We don't try to compare NOFIRSTs since we've changed the string
-                # formatting so the comparison will always fail
-                old_firsts = set(
-                    [f for f in bbox_old["first"] if not f.startswith("NOFIRST")]
-                )
-                new_firsts = set(
-                    [f for f in ri_new["firsts"] if not f.startswith("NOFIRST")]
-                )
-                # print("Old: "); print(old_firsts); print("New: "); print(new_firsts)
-                self.assertEqual(old_firsts, new_firsts)
-
-            logger.warning(f"Test passed for subject {ii}!")
-            ii += 1
+        testutils.assert_json_almost_equal(self, want, got)
 
 
 if __name__ == "__main__":
--- rgz/testutils.py
+++ rgz/testutils.py
@@ -1,8 +1,11 @@
 """Common testing utilities and constants."""
 
 import inspect
+import numbers
 import os
 from pathlib import Path
+from typing import Any
+import unittest
 
 # "Cache" data filename.
 CACHE_DATA_FILENAME = "first"
@@ -28,6 +31,11 @@ WISE_SIA_FILENAME = "wise.fits"
 # Test WISE image filename.
 WISE_IMAGE_FILENAME = "wise_image.fits"
 
+# Valid types that can deserialise from JSON.
+type _JSON_ENTITY = list[_JSON_ENTITY] | dict[
+    str, _JSON_ENTITY
+] | float | int | str | None
+
 
 def get_test_data_dir() -> Path:
     """Gets the directory that test data is held in."""
@@ -47,3 +55,39 @@ def get_wise_sia_file() -> bytes:
 def get_wise_image_file() -> bytes:
     with open(get_test_data_dir() / WISE_IMAGE_FILENAME, "rb") as f:
         return f.read()
+
+
+def _subtractable(a: Any) -> bool:
+    return hasattr(a, "__sub__")
+
+
+def assert_json_almost_equal(t: unittest.TestCase, da: _JSON_ENTITY, db: _JSON_ENTITY):
+    """Asserts that two JSON-deserialised items are almost equal.
+
+    Handles floating point precision.
+
+    Args:
+        da: first item
+        db: second item
+    """
+    # Unfortunately we have to hardcode types here
+    # as strings and lists look ~identical in ducktyping.
+    # At least we only use this for testing!
+    if isinstance(da, dict) and isinstance(db, dict):
+        keys_a, keys_b = da.keys(), db.keys()
+        t.assertEqual(keys_a, keys_b)
+        values_a, values_b = list(da.values()), list(db.values())
+        assert_json_almost_equal(t, values_a, values_b)
+        return
+    if isinstance(da, list) and isinstance(db, list):
+        t.assertEqual(len(da), len(db))
+        for a, b in zip(da, db):
+            assert_json_almost_equal(t, a, b)
+        return
+
+    if _subtractable(da) and _subtractable(db):
+        t.assertAlmostEqual(da, db)  # type: ignore
+        return
+
+    t.assertEqual(da, db)
+    return
